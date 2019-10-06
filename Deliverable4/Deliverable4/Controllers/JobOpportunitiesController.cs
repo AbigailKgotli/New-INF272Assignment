af87053ce6ae9ec467e4268dbc69d18b6ad2fcf0
lebogangmo <u17345180@tuks.co.za>
2019-10-07 01:06:38 +0200

    Job Opportunities

---
+++ Deliverable4/Deliverable4/Controllers/JobOpportunitiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Deliverable4.Controllers
+{
+    public class JobOpportunitiesController : Controller
+    {
+        // GET: JobOpportunities
+        public ActionResult JobOpportunities()
+        {
+            return View();
+        }
+    }
+}
