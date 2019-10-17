798fd8b2a80d2c81d3c7253d4f1a28a571568176
lebogangmo <u17345180@tuks.co.za>
2019-10-17 20:33:52 +0200

    filtering

--- Deliverable4/Deliverable4/Controllers/JobOpportunitiesController.cs
+++ Deliverable4/Deliverable4/Controllers/JobOpportunitiesController.cs
@@ -12,24 +12,25 @@ namespace Deliverable4.Controllers
         // GET: JobOpportunities
         public ActionResult JobOpportunities()
         {
-            JobItDBEntities Opportunities = new JobItDBEntities();
+            JobItDBEntities db= new JobItDBEntities();
             List<SelectListItem> items = new List<SelectListItem>();
-            items.Add(new SelectListItem() { Text = "1", Value = "1" });
-            items.Add(new SelectListItem() { Text = "2", Value = "2" });
-            items.Add(new SelectListItem() { Text = "3", Value = "3" });
-            items.Add(new SelectListItem() { Text = "4", Value = "4" });
-            items.Add(new SelectListItem() { Text = "5", Value = "5" });
-            items.Add(new SelectListItem() { Text = "6", Value = "6" });
-            items.Add(new SelectListItem() { Text = "7", Value = "7" });
-            items.Add(new SelectListItem() { Text = "8", Value = "8" });
-            items.Add(new SelectListItem() { Text = "9", Value = "9" });
-            items.Add(new SelectListItem() { Text = "10", Value = "10" });
-            var ddl = from n in items
-                      select n.Text;
-            ViewBag.No = ddl;
-            JobItDBEntities entities = new JobItDBEntities();
-            var products = (from e in entities.
-                            select e).Take(no);
+            db.Jobs.Where(zz => zz.JobQualifications= "").ToList();
+            //items.Add(new SelectListItem() { Text = "1", Value = "1" });
+            //items.Add(new SelectListItem() { Text = "2", Value = "2" });
+            //items.Add(new SelectListItem() { Text = "3", Value = "3" });
+            //items.Add(new SelectListItem() { Text = "4", Value = "4" });
+            //items.Add(new SelectListItem() { Text = "5", Value = "5" });
+            //items.Add(new SelectListItem() { Text = "6", Value = "6" });
+            //items.Add(new SelectListItem() { Text = "7", Value = "7" });
+            //items.Add(new SelectListItem() { Text = "8", Value = "8" });
+            //items.Add(new SelectListItem() { Text = "9", Value = "9" });
+            //items.Add(new SelectListItem() { Text = "10", Value = "10" });
+            //var ddl = from n in items
+            //          select n.Text;
+            //ViewBag.No = ddl;
+            //JobItDBEntities entities = new JobItDBEntities();
+            //var products = (from e in entities.
+            //                select e).Take(no);
             return View();
         }
        
